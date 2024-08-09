370b9ccc0e0d652f543b9cdd1b8f264fea72506b
synthful <59634395+kyle1373@users.noreply.github.com>
2024-08-09 01:26:10 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    experimental: {
+      appDir: true,
+    },
+    images: { unoptimized: true },
+    assetPrefix: "https://personal-pt.github.io/",
+  }
 
 export default nextConfig;
